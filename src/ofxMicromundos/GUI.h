02d4f6aea5bdb187e3c49242be17dbf4a7c0766e
Christian Parsons <chparsons@gmail.com>
2018-01-23 22:05:46 -0300

    ofxPlab

---
+++ src/ofxMicromundos/GUI.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxGui.h"
+
+class GUI
+{
+  public:
+
+    ofParameter<bool> backend_monitor;
+    ofParameter<bool> send;
+
+    void init_params()
+    {
+      p.add( backend_monitor
+          .set("backend_monitor", true) );
+
+      p.add( send
+          .set("send", true) );
+    }; 
+
+    void init(float w)
+    {
+      ofxBaseGui::setDefaultWidth(w);
+      p.setName("params");
+      init_params();
+      gui.setup(p, "server_gui.xml");
+      gui.loadFromFile("server_gui.xml");
+    }; 
+
+    void render(float x, float y)
+    { 
+      gui.setPosition(x, y);
+      gui.draw();
+    };
+
+  private:
+
+    ofParameterGroup p;
+    ofxPanel gui; 
+};
+
